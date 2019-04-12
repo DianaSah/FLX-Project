891ab30500e436ee7b3af36f9983b7d5e33e2a88
Diana Sahan <sahandianaa@gmail.com>
2019-04-12 19:49:22 +0200

    nav with routing (not completed yet)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { RecipesListComponent } from './components/recipes-list/recipes-list.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'recipes_list', component: RecipesListComponent},
+  {path: 'profile', component: ProfileComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
