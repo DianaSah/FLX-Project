2210db0896bf7d90874b303e6cf01a1da28a055b
SavchenkovaJulia <sawjulia9@gmail.com>
2019-04-13 18:42:24 +0300

    add(mock-recipes)
    add local demo database and recipe model

---
+++ src/app/models/recipe.ts
@@ -0,0 +1,12 @@
+export class Recipe {
+    id: number;
+    title: string;
+    imageSrc: string;
+    description: string;
+    cuisineType: string;
+    rating: number;
+    isFavorite: boolean;
+    cookDuration: number;
+    ingredients: string[];
+    steps: string;
+};
