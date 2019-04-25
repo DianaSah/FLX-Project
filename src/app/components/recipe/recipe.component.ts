c277370dcab664618304bb2c2dfde7cc38f013a2
7vovk <7vovk@rambler.ru>
2019-04-25 10:26:39 +0300

    fix(fav btn): bug when automatically likes and dislikes post when it's opened

--- src/app/components/recipe/recipe.component.ts
+++ src/app/components/recipe/recipe.component.ts
@@ -23,12 +23,10 @@ export class RecipeComponent implements OnInit {
 
   ngOnInit(): void {
     this.getRecipe();
-    if (!this.recipe.isFavorite) {
+    if (this.recipe.isFavorite === false) {
       this.favorite = 'favorite_border';
-      this.recipe.isFavorite = true;
-    } else {
+    } else if (this.recipe.isFavorite === true) {
       this.favorite = 'favorite';
-      this.recipe.isFavorite = false;
     }
   }
 
@@ -42,12 +40,13 @@ export class RecipeComponent implements OnInit {
     this.location.back();
   }
 
-
   handleFavorite() {
     if (this.favorite === 'favorite_border') {
       this.favorite = 'favorite';
+      this.recipe.isFavorite = true;
     } else {
       this.favorite = 'favorite_border';
+      this.recipe.isFavorite = false;
     }
   }
 
