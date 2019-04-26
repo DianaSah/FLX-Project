5def98521194d84030165ac4acf54af86ad77294
Diana Sahan <sahandianaa@gmail.com>
2019-04-26 18:31:02 +0300

    fix(modal window add new recipe works but without firebase)

--- src/app/components/add-new-recipe/add-new-recipe.component.ts
+++ src/app/components/add-new-recipe/add-new-recipe.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {RECIPES} from '../../mock_recepies';
 import {NgForm} from '@angular/forms';
+import { RecipesListComponent } from '../recipes-list/recipes-list.component';
+import { getInjectorIndex } from '@angular/core/src/render3/di';
 
 
 @Component({
@@ -9,11 +11,30 @@ import {NgForm} from '@angular/forms';
   styleUrls: ['./add-new-recipe.component.scss']
 })
 export class AddNewRecipeComponent implements OnInit {
-  recipes = RECIPES;
+  
+  genId(){
+    return (RECIPES.length +1);
+  }
 
   onSubmitNewRecipe(value: any) {
-    console.log(value);  
+    
+    let CustomRecipe = {
+      id: this.genId(),
+      title: value.title,
+      imageSrc: '',
+      description: value.description,
+      cuisineType: value.cuisineType,
+      rating: 0,
+      isFavorite: false,
+      cookDuration: value.cookDuration,
+      ingredients: value.ingredients,
+      steps: value.steps
+    };
+    console.log(CustomRecipe);
+    RECIPES[RECIPES.length] = CustomRecipe;
+    console.log(RECIPES);
   }
+  
   constructor() { }
 
   ngOnInit() {
