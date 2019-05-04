568d3e9fec7f415029d55b98190ce69aec434888
SavchenkovaJulia <sawjulia9@gmail.com>
2019-05-04 16:27:15 +0300

    fix(login-form)
    feat/login-form: relocate firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,12 +36,13 @@ import { SearchByIngredientsComponent } from './components/search-by-ingredients
     MatInputModule,
     MatIconModule,
     NgxAuthFirebaseUIModule.forRoot({
-      apiKey: "AIzaSyDFRKhQ0MIfPRQz8htnyBntkpChVkLYiDE",
-      authDomain: "cookbook-bf1ce.firebaseapp.com",
-      databaseURL: "https://cookbook-bf1ce.firebaseio.com",
-      projectId: "cookbook-bf1ce",
-      storageBucket: "cookbook-bf1ce.appspot.com",
-      messagingSenderId: "77682848734"
+    apiKey: "AIzaSyAPCaQl95Xh3q9DS8VhATyLxwKKuGzmick",
+    authDomain: "cookbook-epam2019.firebaseapp.com",
+    databaseURL: "https://cookbook-epam2019.firebaseio.com",
+    projectId: "cookbook-epam2019",
+    storageBucket: "cookbook-epam2019.appspot.com",
+    messagingSenderId: "21753682188",
+    appId: "1:21753682188:web:1888b214325b993f"
     }),
 
   ],
