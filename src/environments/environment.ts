2f3495b1635ebd64f80d644e83f66b29ccbf3e8e
Diana Sahan <sahandianaa@gmail.com>
2019-05-04 17:00:35 +0300

    fix(change on new firebase)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyD6Yy9umqLR1vAl3FTJbYMsw9jEa6ocGEg",
-    authDomain: "cookbook-1ec11.firebaseapp.com",
-    databaseURL: "https://cookbook-1ec11.firebaseio.com",
-    projectId: "cookbook-1ec11",
-    storageBucket: "cookbook-1ec11.appspot.com",
-    messagingSenderId: "483260807213"
+    apiKey: "AIzaSyAPCaQl95Xh3q9DS8VhATyLxwKKuGzmick",
+    authDomain: "cookbook-epam2019.firebaseapp.com",
+    databaseURL: "https://cookbook-epam2019.firebaseio.com",
+    projectId: "cookbook-epam2019",
+    storageBucket: "cookbook-epam2019.appspot.com",
+    messagingSenderId: "21753682188",
+    appId: "1:21753682188:web:1888b214325b993f"
   }
 };
 
