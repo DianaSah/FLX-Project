3263bac8370e26918ea167a31330441fbc9b31db
SavchenkovaJulia <sawjulia9@gmail.com>
2019-05-14 21:15:31 +0300

    fix(profile):fix icons toggle

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatDialog, MatDialogConfig} from '@angular/material';
-import { AddNewRecipeComponent } from '../add-new-recipe/add-new-recipe.component';
-//import {Recipe} from '../../models/recipe' 
-
+import {AddNewRecipeComponent} from '../add-new-recipe/add-new-recipe.component';
+//import {Recipe} from '../../models/recipe'
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-profile',
@@ -11,15 +11,22 @@ import { AddNewRecipeComponent } from '../add-new-recipe/add-new-recipe.componen
 })
 export class ProfileComponent implements OnInit {
   //userRecipe: Recipe = {}
+  isLoggedIn: boolean;
 
   constructor(
-    private dialog: MatDialog) { }
+    private dialog: MatDialog,
+  ) { }
 
   ngOnInit() {
+    firebase.auth().onAuthStateChanged(user => {
+      user ? this.isLoggedIn = true : this.isLoggedIn = false;
+    });
   }
 
   addCustomerRecipe() {
     this.dialog.open(AddNewRecipeComponent);
   }
+
 }
+
 
