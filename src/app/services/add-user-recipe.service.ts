0144545c8ef91badcbdea1b7490f0dcc5c8dcce5
SavchenkovaJulia <sawjulia9@gmail.com>
2019-05-20 00:09:20 +0300

    fix(add-new-recipe):fix service and recipe details

---
+++ src/app/services/add-user-recipe.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore} from '@angular/fire/firestore';
+import * as firebase from 'firebase/app';
+import { Recipe } from '../models/recipe';
+import { map } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddUserRecipeService {
+
+  constructor(public afs: AngularFirestore) { }
+  getRecipes(userId) {
+    return this.afs.collection('users').doc(userId).collection<Recipe>('userRecipes').snapshotChanges().pipe(
+      map(actions => actions.map(a => {
+        const data = a.payload.doc.data() as Recipe;
+        const id = a.payload.doc.id;
+        return { id, ...data };
+      }))
+    );
+  }
+  addUserRecipe(userRecipe, recipeId) {
+    firebase.auth().onAuthStateChanged((user) => {
+      this.afs.collection('users').doc(user.uid).collection('userRecipes').doc((recipeId)).set({
+        title: userRecipe.title,
+        imageSrc: userRecipe.imageSrc,
+        description: userRecipe.description,
+        cuisineType: userRecipe.cuisineType,
+        rating: 0,
+        isFavorite: false,
+        cookDuration: parseInt(userRecipe.cookDuration),
+        ingredients: userRecipe.ingredients,
+        steps: userRecipe.steps,
+        //videos: value.videos,
+      })
+    });
+  }
+}
